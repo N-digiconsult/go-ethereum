3ab54a5f1e9a8d3117a0a3bfadd2fd457851d521
nDigiconsult <191206568+N-digiconsult@users.noreply.github.com>
2025-11-04 22:39:03 +0100

    Remove unused methods from senderFromServer
    Removed methods from senderFromServer that handle signing and equality checks.

--- ethclient/signer.go
+++ ethclient/signer.go
@@ -38,25 +38,3 @@ func setSenderFromServer(tx *types.Transaction, addr common.Address, block commo
 	// Use types.Sender for side-effect to store our signer into the cache.
 	types.Sender(&senderFromServer{addr, block}, tx)
 }
-
-func (s *senderFromServer) Equal(other types.Signer) bool {
-	os, ok := other.(*senderFromServer)
-	return ok && os.blockhash == s.blockhash
-}
-
-func (s *senderFromServer) Sender(tx *types.Transaction) (common.Address, error) {
-	if s.addr == (common.Address{}) {
-		return common.Address{}, errNotCached
-	}
-	return s.addr, nil
-}
-
-func (s *senderFromServer) ChainID() *big.Int {
-	panic("can't sign with senderFromServer")
-}
-func (s *senderFromServer) Hash(tx *types.Transaction) common.Hash {
-	panic("can't sign with senderFromServer")
-}
-func (s *senderFromServer) SignatureValues(tx *types.Transaction, sig []byte) (R, S, V *big.Int, err error) {
-	panic("can't sign with senderFromServer")
-}
